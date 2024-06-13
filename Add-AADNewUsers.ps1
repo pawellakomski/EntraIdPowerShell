a5de67d3cf92aa21ba71bdddc883b4625cf59252
Pawel Lakomski <34425146+pawellakomski@users.noreply.github.com>
2024-06-13 12:58:51 +0200

    Create Add-AADNewUsers.ps1

---
+++ Add-AADNewUsers.ps1
@@ -0,0 +1,28 @@
+<#
+    .SYNOPSIS
+    Add-AADNewUsers.ps1
+
+    .DESCRIPTION
+    Dodawanie użytkowników utworzonych w przeciągu ostatnich 24h do okreslonej grupy AAD. Przed użyciem skonfiguruj właściwe uprawnienia na Graph API i we właściwy sposób użyj Connect-MgGraph. Wymagany scope: Application.ReadWrite.All, Directory.Read.All, GroupMember.ReadWrite.All, User.Read.All
+    Ustaw właściwe ID grupy Entra ID w linii 25.
+
+    .LINK
+    #
+
+    .NOTES
+    Autor:      Paweł Łakomski
+#>
+
+# Przed użyciem skonfiguruj właściwe uprawnienia na Graph API i we właściwy sposób użyj Connect-MgGraph. Wymagany scope: Application.ReadWrite.All, Directory.Read.All, GroupMember.ReadWrite.All, User.Read.All
+# Connect-MgGraph
+
+#ilosc dni wstecz, które sprawdzamy - do ustalenia/sprawdzenia
+$wczoraj = (Get-date).AddDays(-2).ToString("yyyy-MM-dd'T'HH:mm:ss.fffffff'Z'")
+$users = Get-MgUser -All -Filter "CreatedDateTime ge $wczoraj"
+
+foreach($user in $users)
+{
+    New-MgGroupMember -GroupId '68fa1256-8c38-42b3-a1c2-b22939758671' -DirectoryObjectId $user.Id
+}
+
+Disconnect-MgGraph
